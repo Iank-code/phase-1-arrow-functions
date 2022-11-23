bcff315e47f1ca03bd9ac6d1ea7c51bc1be02789
ianK-code <kian99564@gmail.com>
2022-11-23 15:21:16 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(){
+
+    const divideRem = 2000 / 100
+    let squareFunc = (x)=> {return x * 2}
+    squareFunc(2)
+    return divideRem
+}
+divide()
+let x = 2
+const square = (x) => {
+    return Math.pow(x, 2)
+}
+let a = 3
+let b = 4
+const add = (a, b) => {
+    return a + b
+}
